4e7629551d522f7a1579a144b39a127ee25a08e7
Yaroslav Prokopovych <yarkopro@gmail.com>
2018-06-12 21:30:57 +0300

    Create Spring Boot project

---
+++ api/src/main/java/com/yarkopro/samplewebapp/SamplewebappApplication.java
@@ -0,0 +1,12 @@
+package com.yarkopro.samplewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplewebappApplication.class, args);
+	}
+}
