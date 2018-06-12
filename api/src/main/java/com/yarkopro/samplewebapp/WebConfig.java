0b0aca6edbd28ed30293bc9a9797e517ba1cf0e8
Yaroslav Prokopovych <yarkopro@gmail.com>
2018-06-13 02:13:06 +0300

    Update Webconfig

--- api/src/main/java/com/yarkopro/samplewebapp/WebConfig.java
+++ api/src/main/java/com/yarkopro/samplewebapp/WebConfig.java
@@ -1,11 +1,13 @@
 package com.yarkopro.samplewebapp;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan("com.yarkopro.samplewebapp.*")
 public class WebConfig implements WebMvcConfigurer {
 
 }
